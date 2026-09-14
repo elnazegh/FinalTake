cdd5b0168f75502d23f41b1326ef23f665b22445
RickyPham1 <rickytp98787@csu.fullerton.edu>
2026-09-13 20:19:32 -0700

    Establish MySQL Connection

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,22 @@
 from flask import Flask
+from database import get_db_connection
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return "FinalTake backend is running!"
 
-if __name__ == '__main__':
+@app.route("/")
+def home():
+    connection = get_db_connection()
+
+    if connection.is_connected():
+        message = "FinalTake backend and MySQL are connected!"
+    else:
+        message = "Database connection failed."
+
+    connection.close()
+
+    return message
+
+
+if __name__ == "__main__":
     app.run(debug=True)
