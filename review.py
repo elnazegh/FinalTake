b66e9cef28b84abcfb180613c4edf89ace6d97a8
Colby-Kern <kern.colby27@csu.fullerton.edu>
2026-09-16 06:26:47 -0700

    Implement Review class with validation for text

---
+++ review.py
@@ -0,0 +1,31 @@
+class Review:
+    MAX_LENGTH = 5000
+
+    def __init__(self, user_id, media_id, rating, text):
+        self.user_id = user_id
+        self.media_id = media_id
+        self.rating = rating
+        self.text = None
+
+        self.set_text(text)
+
+    def set_text(self, text):
+        """Set the review text."""
+
+        if not isinstance(text, str):
+            raise ValueError("Review must be text.")
+
+        text = text.strip()
+
+        if not text:
+            raise ValueError("Review cannot be empty.")
+
+        if len(text) > self.MAX_LENGTH:
+            raise ValueError(
+                f"Review cannot exceed {self.MAX_LENGTH} characters."
+            )
+
+        self.text = text
+
+    def __str__(self):
+        return self.text
