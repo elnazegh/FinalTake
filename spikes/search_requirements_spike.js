4ce959330164305fdef7c323686a2ec7243d3f91
Yuuji <kobayashi.08120509@gmail.com>
2026-09-16 01:18:39 -0700

    Expand search spike test coverage

--- spikes/search_requirements_spike.js
+++ spikes/search_requirements_spike.js
@@ -2,7 +2,8 @@ const media = [
     { id: "1", title: "The Dark Knight", type: "movie" },
     { id: "2", title: "Dark", type: "tv" },
     { id: "3", title: "Darkest Hour", type: "movie" },
-    { id: "4", title: "Dune", type: "book" }
+    { id: "4", title: "Dune", type: "book" },
+    { id: "5", title: "Dark Souls", type: "game" },
 ];
 
 function searchByTitle(query) {
@@ -19,5 +20,5 @@ function searchByTitle(query) {
 
 console.log(searchByTitle("dark"));
 console.log(searchByTitle("DARK"));
-console.log(searchByTitle("Dark"));
+console.log(searchByTitle("xyz"));
 console.log(searchByTitle(""));
